0e39ecac1f881c642a6e05064de686a51cb53bdc
Hannakua <126361524+Hannakua@users.noreply.github.com>
2023-05-22 19:53:54 +0300

    Update HW_11_main.py

--- HW_11_main.py
+++ HW_11_main.py
@@ -1,5 +1,5 @@
 import sys
-from classes_2 import AddressBook, Record, Phone, Birthday, Iterable
+from classes import AddressBook, Record, Phone, Birthday, Iterable
 
 USERS = AddressBook()
   
@@ -9,13 +9,13 @@ def error_handler(func):
             result = func(*args)
             return result
         except KeyError:
-            return "No such user."
+            return "No such user"
         except ValueError:
-            return 'Please, give me the name and phone number(38XXX-XXX-XX or XXX-XXX-XX-XX) or birth in format dd/mm/yyyy.'
+            return 'Please, give me phone number(38XXX-XXX-XX or XXX-XXX-XX-XX) or birth in format dd/mm/yyyy.'
         except IndexError:
-            return 'Enter user name.'
+            return 'Enter user name'
         except TypeError:
-            return "Example: add Tomas, 0506936985"
+            return "Phone: XXX-XXX-XX-XX, Date: dd/mm/yyyy"
     return inner
 
 
@@ -30,65 +30,60 @@ def unknown_command():
 
 
 def help():
-    return "I am a bot. I accept the following commands: 'hello', 'add', 'change', 'show all', 'print' 'exit', 'goodbye', 'close', 'birth'. For 'add' or 'change' commands please enter the command in sequence: command user_name comma backspace user_number. For example: 'add Taras Kovalenko, 0676542345'"
+    return "I am a bot. I accept the following commands: 'hello', 'add', 'change', 'show all', 'print pages', 'birth', 'exit', 'goodbye', 'close'. For 'add' or 'change' commands please enter the command in sequence: command user_name comma backspace user_number. For example: 'add Taras Kovalenko, 0676542345'"
 
 @error_handler
-# def add_user(name, phone, birth):
-def add_user(name, phone):
-    
-    user = Record(name, phone)
-
-    # user = Record(name, phone, birth)
-    user.add_phone(phone)    
+def add_user(name, phone, birth = None):     # add user_name, phone_number, birthday(or not)
+    if name not in USERS.data:
+        user = Record(name, phone, birth)
+    else:
+        user = USERS[name]
+        user.add_phone(phone)
+        if birth is not None:
+            user.add_birthday(birth)
     USERS.add_record(user)
-
-    # record = Record(name, phone)
-    # record.add_phone(phone)
-    # result = USERS.add_record(record)
-  
     result = user.user_record
     return result
 
-# @error_handlers
-def add_birth(name, birth):
-    user = USERS[name]
-    user.user_record.add_birthday(birth)
+@error_handler
+def add_birth(name, birth, phone = None) :           # birth user_name, dd/mm/yyyy
+    if name in USERS.data:
+        user = USERS[name]
+        user.add_birthday(birth)
+    else:
+        user = Record(name, phone, birth)
+    USERS.add_record(user)
     return user.user_record
 
+def days_to_birth(name):    # next birth user_name
+    user = USERS[name]
+    result = user.days_to_birthday()    
+    return f"{result} days"
 
 @error_handler
-def change_phone(name, new_num):
-    
-    if name in USERS.keys():        
-        USERS.pop(name)      
-    record = Record(name, new_num)
-    record.edit_phone(new_num)
-    result = USERS.add_record(record) 
-    # old_num=USERS[name]
-    
-    return f'New phone number {new_num} is saved for {name}. Old numbers were deleted. \n{result}'
+def change_phone(name, old_num, new_num):   # change user_name, old_number, new_number      
+    user = USERS[name]     
+    user.edit_phone(old_num, new_num)
+    USERS.add_record(user)
+    return user.user_record    
 
 @error_handler
-def phone_show(name):
-    # name = ''.join(name)
-    number = USERS.get(name, f'user is not in phone book yet')
-    return f'Phone number for user {name} is: {number}.'
+def phone_show(name):        #phone show user_name
+    user = USERS[name]
+    result = user.show_phones()
+    return result
 
-
-def show_all():
-    # result = '\n'
+def show_all():  # show all
     if USERS == {}:
         result = "Not any record in phone book yet."
     else:
         USERS.show_all()
 
-def show_part(n):
+def show_part(n):  # print pages n
     iter = Iterable(int(n), USERS.data)
     for i in iter:
         print(i)
-
-    
-
+   
 def exit():
     print('Good Bye!') 
     return sys.exit()
@@ -103,7 +98,8 @@ HANDLERS = {
     'good bye': exit,
     'change': change_phone,  
     'birth': add_birth, 
-    'print': show_part,
+    'print pages': show_part,
+    'next birth': days_to_birth,
 }
 
 
@@ -122,8 +118,7 @@ def identeficate_command(user_input):
 
     if data:
         data = data[0].split(',')
-    print(data)
-        
+  
     return handler, data
 
 
